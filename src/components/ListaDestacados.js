e1eb0cb0e1dfd70db99a58af93667a9e55212cae
Carlos <calandaeta.17@est.ucab.edu.ve>
2020-11-17 10:35:27 -0400

    La lista es cliqueable

--- src/components/ListaDestacados.js
+++ src/components/ListaDestacados.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Divider from "@material-ui/core/Divider";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,12 +31,14 @@ export default function SpacingGrid() {
             {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => (
               <Grid key={value} item>
                 <Paper className={classes.paper} class="border rounded">
-                  <img
-                    src="https://http2.mlstatic.com/televisor-aiwa-32-led-hd-hdmi-isdbt-D_NQ_NP_766490-MLV43440988575_092020-W.webp"
-                    class="tamañoLista"
-                    alt=""
-                  />
-                  Producto {value}
+                  <Button>
+                    <img
+                      src="https://http2.mlstatic.com/televisor-aiwa-32-led-hd-hdmi-isdbt-D_NQ_NP_766490-MLV43440988575_092020-W.webp"
+                      class="tamañoLista"
+                      alt=""
+                    />
+                    Producto {value}
+                  </Button>
                 </Paper>
               </Grid>
             ))}
