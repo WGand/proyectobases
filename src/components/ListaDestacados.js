51d438b69a5f2161fef80672fcf871385e79d8ad
Carlos <calandaeta.17@est.ucab.edu.ve>
2020-11-17 22:55:27 -0400

    Cambios pequeños a la lista

--- src/components/ListaDestacados.js
+++ src/components/ListaDestacados.js
@@ -30,16 +30,14 @@ export default function SpacingGrid() {
           <Grid container justify="center" spacing={spacing}>
             {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => (
               <Grid key={value} item>
-                <Paper className={classes.paper} class="border rounded">
-                  <Button>
-                    <img
-                      src="https://http2.mlstatic.com/televisor-aiwa-32-led-hd-hdmi-isdbt-D_NQ_NP_766490-MLV43440988575_092020-W.webp"
-                      class="tamañoLista"
-                      alt=""
-                    />
-                    Producto {value}
-                  </Button>
-                </Paper>
+                <Button variant="outlined">
+                  <img
+                    src="https://http2.mlstatic.com/televisor-aiwa-32-led-hd-hdmi-isdbt-D_NQ_NP_766490-MLV43440988575_092020-W.webp"
+                    class="tamañoLista"
+                    alt=""
+                  />
+                  Producto {value}
+                </Button>
               </Grid>
             ))}
           </Grid>
